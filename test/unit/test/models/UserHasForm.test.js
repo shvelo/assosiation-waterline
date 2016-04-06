18ebf254aa3666949e7c3c86703537fd23b9e497
Nick Shvelidze <n.shvelidze@in2circle.com>
2016-04-06 11:53:22 +0400

    Add test for user save

--- test/unit/test/models/UserHasForm.test.js
+++ test/unit/test/models/UserHasForm.test.js
@@ -40,4 +40,24 @@ describe('UserHasForm', function () {
       }).catch(done);
   });
 
+	it('retains populated associations after save', function (done) {
+    UserHasForm.create({
+        to_edit: true,
+        to_delete: true,
+        user_id: user.id,
+        form_id: form.id
+      })
+      .then(function (createdRecord){
+        return User.find().populateAll();
+      })
+      .then(function (userFound){
+        console.log(userFound);
+        assert(userFound[0].forms.length, 'user has forms before save');
+				return userFound[0].save().then(function(){
+	        assert(userFound[0].forms.length, 'user has forms after save');
+        	done();
+				});
+      }).catch(done);
+  });
+
 });
